389042ae291c1e3f709fb0782e7f1d4a9d5fa3cb
Wellington Mariano Pedro <113481316+Well-Mariano@users.noreply.github.com>
2025-09-27 23:48:44 -0300

    Update visualizacao_graficos.R

--- visualizacao_graficos.R
+++ visualizacao_graficos.R
@@ -1,1 +1,33 @@
-# Script de visualização de gráficos do CAGED
+### Exemplos de Gráficos
+
+# Gráfico Saldo por regiões do Brasil
+caged %>%
+  group_by(região) %>%
+  summarise(saldo = sum(saldomovimentação, na.rm = T)) %>% 
+  filter(região != "Desconhecido") %>% 
+  mutate(região = fct_reorder(região, saldo)) %>%
+  ggplot(aes(x = região, y = saldo, fill = região)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Saldo das Movimentações por Região",
+       caption = "Ministério do Trabalho e Emprego (MTE)",
+       x = "Região", 
+       y = "Saldo Movimentação") +
+  theme_minimal() +
+  coord_flip() +
+  geom_label(aes(label = saldo), hjust = 0.75, show.legend = F)
+
+# Gráfico Saldo por regiões do Brasil
+caged %>%
+  group_by(região) %>%
+  summarise(saldo = sum(saldomovimentação, na.rm = T)) %>% 
+  filter(região != "Desconhecido") %>% 
+  mutate(região = fct_reorder(região, saldo)) %>%
+  ggplot(aes(x = região, y = saldo, fill = região)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Saldo das Movimentações por Região",
+       caption = "Ministério do Trabalho e Emprego (MTE)",
+       x = "Região", 
+       y = "Saldo Movimentação") +
+  theme_minimal() +
+  coord_flip() +
+  geom_label(aes(label = saldo), hjust = 0.75, show.legend = F)
